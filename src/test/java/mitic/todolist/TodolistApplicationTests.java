09b506839b2199a97308c21b0095625331d4269c
mohamed-sanogo <euromoh68@gmail.com>
2024-08-20 15:05:56 +0000

    init

---
+++ src/test/java/mitic/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package mitic.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
